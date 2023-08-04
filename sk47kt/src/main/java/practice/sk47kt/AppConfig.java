6597ddc9cd7e66b59100f77d5e77a95b2a7aed90
sk47kt <sk47kt@naver.com>
2023-08-04 16:06:49 +0900

    Singleton Appconfig

--- sk47kt/src/main/java/practice/sk47kt/AppConfig.java
+++ sk47kt/src/main/java/practice/sk47kt/AppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import practice.sk47kt.discount.DiscountPolicy;
 import practice.sk47kt.discount.FixDiscountPolicy;
 import practice.sk47kt.discount.RateDiscountPolicy;
+import practice.sk47kt.member.MemberRepository;
 import practice.sk47kt.member.MemberService;
 import practice.sk47kt.member.MemberServiceImpl;
 import practice.sk47kt.member.MemoryMemberRepository;
@@ -16,16 +17,19 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemoryMemberRepository memberRepository() {
+    public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
---
+++ sk47kt/src/test/java/practice/sk47kt/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,40 @@
+package practice.sk47kt.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import practice.sk47kt.AppConfig;
+import practice.sk47kt.member.MemberRepository;
+import practice.sk47kt.member.MemberServiceImpl;
+import practice.sk47kt.order.OrderServiceImpl;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void ConfigurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        System.out.println("memberRepository1 = " + memberRepository1);
+        System.out.println("memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2).isSameAs(memberRepository);
+    }
+
+    @Test
+    void ConfigurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass()); //CGLIB
+    }
+}
